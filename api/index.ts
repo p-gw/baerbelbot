8b8ebde9d46f2f103eff761ba7cbb1d16755c715
Philipp Gewessler <p.gewessler@gmx.at>
2022-05-14 09:27:51 +0200

    add firstseen to api response

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 require("dotenv").config()
-import { User, PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 import express from "express"
 import { weeklyAmount } from "../discord/utils"
 const prisma = new PrismaClient()
@@ -8,30 +8,38 @@ const api = express()
 const PORT = process.env.API_PORT
 
 api.get("/weekly", async (req, res) => {
-  const weeklyAmounts = await weeklyAmount({}, ["userId"], false)
+    const weeklyAmounts = await weeklyAmount({}, ["userId"], false)
 
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
-      username: true
-    }
-  })
-
-  let responseData: { userId: string | undefined, username: string | undefined, date: Date, amount: number }[] = [];
-
-  for (let amount of weeklyAmounts) {
-    const user = users.find(user => user.id === amount.userId)
-    responseData.push({
-      userId: amount.userId,
-      username: user ? user.username : undefined,
-      date: amount.date,
-      amount: amount.amount
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            username: true,
+            firstSeen: true
+        }
     })
-  }
 
-  res.send(responseData)
+    let responseData: {
+        userId: string | undefined,
+        username: string | undefined,
+        firstSeen: Date | null | undefined,
+        date: Date,
+        amount: number
+    }[] = [];
+
+    for (let amount of weeklyAmounts) {
+        const user = users.find(user => user.id === amount.userId)
+        responseData.push({
+            userId: amount.userId,
+            username: user?.username,
+            firstSeen: user?.firstSeen,
+            date: amount.date,
+            amount: amount.amount
+        })
+    }
+
+    res.send(responseData)
 })
 
 api.listen(PORT, () => {
-  console.log(`Server is running at https://localhost:${PORT}`)
+    console.log(`Server is running at https://localhost:${PORT}`)
 })
